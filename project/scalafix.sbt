8b49419236ea3a171f3a8d71a1ee3ec30b238446
Devon Stewart <dstewart@twilio.com>
2021-03-15 23:28:11 -0700

    Moderninzing repo

--- project/scalafix.sbt
+++ project/scalafix.sbt
@@ -1,2 +1,1 @@
-addSbtPlugin("com.twilio" % "sbt-guardrail" % "0.57.2")
 addSbtPlugin("ch.epfl.scala" % "sbt-scalafix" % "0.9.17")
--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,56 +1,28 @@
+import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent._
-import nullability.pet._
-import nullability.definitions.Pet
+import scala.concurrent.duration.Duration
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.ContentType
+
+import foo.pet._
+import foo.definitions.Pet
 
 object App extends App {
-  import scala.concurrent.ExecutionContext.Implicits.global
 
-  var actorSystem: Option[(akka.actor.ActorSystem, akka.stream.ActorMaterializer)] = None
-  var binding: Option[akka.http.scaladsl.Http.ServerBinding] = None
-
-  def getActorSystem(): (akka.actor.ActorSystem, akka.stream.ActorMaterializer) = {
-    actorSystem.getOrElse {
-      implicit val localActorSystem = akka.actor.ActorSystem()
-      val mat = akka.stream.ActorMaterializer()
-      actorSystem = Some((localActorSystem, mat))
-      (localActorSystem, mat)
-    }
-  }
-
-  implicit def localActorSystem: akka.actor.ActorSystem = getActorSystem()._1
-  implicit def localMaterializer: akka.stream.ActorMaterializer = getActorSystem()._2
-
-  def unbind(): Unit = {
-    import scala.concurrent._
-    import scala.concurrent.duration.Duration
-    binding.foreach { x =>
-      Await.result(x.unbind(), Duration.Inf)
-      binding = None
-    }
-  }
-
-  def bindAndServe(value: akka.http.scaladsl.server.Route): Unit = {
-    import scala.concurrent._
-    import scala.concurrent.duration._
-    unbind()
-    binding = Some(Await.result(akka.http.scaladsl.Http().bindAndHandle(value, "127.0.0.1", 8080), Duration.Inf))
-    println("Running at http://localhost:8080 !")
-  }
+  implicit def actorSystem = ActorSystem()
 
   val routes = PetResource.routes(new PetHandler {
-    override def createPet(respond: PetResource.CreatePetResponse.type)(name: Option[String], status: Option[String], file: Option[(java.io.File, Option[String], akka.http.scaladsl.model.ContentType)]): scala.concurrent.Future[PetResource.CreatePetResponse] = {
+    override def createPet(respond: PetResource.CreatePetResponse.type)(name: Option[String], status: Option[String], file: Option[(java.io.File, Option[String], ContentType)]): scala.concurrent.Future[PetResource.CreatePetResponse] = {
       Future.successful(respond.OK(Pet()))
     }
-    override def createPetMapFileField(fieldName: String,fileName: Option[String],contentType: akka.http.scaladsl.model.ContentType): java.io.File = ???
+    override def createPetMapFileField(fieldName: String,fileName: Option[String],contentType: ContentType): java.io.File = ???
     override def updatePet(respond: PetResource.UpdatePetResponse.type)(name: String, body: Option[Pet]): Future[PetResource.UpdatePetResponse] = {
-      Future.successful(Pet())
+      Future.successful(respond.OK(body.getOrElse(Pet())))
     }
   })
-  bindAndServe(routes)
 
-  Future {
-    Thread.sleep(15 * 1000)
-    unbind()
-    localActorSystem.terminate()
-  }
+  Await.result(Http().bindAndHandle(routes, "127.0.0.1", 8080), Duration.Inf)
+  println("Running at http://localhost:8080 !")
 }
