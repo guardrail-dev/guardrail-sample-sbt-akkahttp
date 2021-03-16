540d822fb6763d819f4116799cdb7aa6673ace95
Devon Stewart <dstewart@twilio.com>
2021-03-15 23:22:28 -0700

    Updating repo

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -38,11 +38,11 @@ object App extends App {
   }
 
   val routes = PetResource.routes(new PetHandler {
-    override def createPet(respond: PetResource.createPetResponse.type)(name: Option[String], status: Option[String], file: Option[(java.io.File, Option[String], akka.http.scaladsl.model.ContentType)]): scala.concurrent.Future[PetResource.createPetResponse] = {
+    override def createPet(respond: PetResource.CreatePetResponse.type)(name: Option[String], status: Option[String], file: Option[(java.io.File, Option[String], akka.http.scaladsl.model.ContentType)]): scala.concurrent.Future[PetResource.CreatePetResponse] = {
       Future.successful(respond.OK(Pet()))
     }
     override def createPetMapFileField(fieldName: String,fileName: Option[String],contentType: akka.http.scaladsl.model.ContentType): java.io.File = ???
-    override def updatePet(respond: PetResource.updatePetResponse.type)(name: String, body: Option[Pet]): Future[PetResource.updatePetResponse] = {
+    override def updatePet(respond: PetResource.UpdatePetResponse.type)(name: String, body: Option[Pet]): Future[PetResource.UpdatePetResponse] = {
       Future.successful(Pet())
     }
   })
